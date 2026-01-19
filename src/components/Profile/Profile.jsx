b1280812d0ac4896a8ac36f30b352365b90f6adb
Guilherme <gfernandoportelaoliveira@gmail.com>
2026-01-19 16:59:03 -0300

    Ajustes para o deploy

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,16 @@
-import './style.css'
-
+import './style.css'; 
 
 function Profile() {
     return(
         <section>
             <div className='perfil'>
-             <img src="./public/FotoGui/foto_guilherme.jpg" alt="Guilherme" />
-             <p>Buscando unir lógica e design para criar experiências digitais únicas.</p>
+                <img src="/FotoGui/foto_guilherme.jpg" alt="Guilherme" />
+                <p>Buscando unir lógica e design para criar experiências digitais únicas.</p>
             </div>
         </section>
     )
 }
 
 export default Profile;
+
 
