e66ad0b99a15c71c5ed92213eb4750d4c932c843
Guilherme <gfernandoportelaoliveira@gmail.com>
2026-01-21 13:35:08 -0300

    Add h2 no profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,30 @@
 import './style.css'; 
+import { useState, useEffect } from 'react';
 
 function Profile() {
+   const text = "Desenvolvedor de Software Multiplataforma"
+   const [textPerfil, setTextPerfil] = useState("")
+   const [index, setIndex] = useState(0)
+
+   useEffect(() => {
+     if (index < text.length) {
+       const timeout = setTimeout(() => {
+       setTextPerfil((prev) => prev + text.charAt(index))
+       setIndex(index + 1)
+     }, 120)
+
+    return () => clearTimeout(timeout)
+  }
+}, [index, text])
+
     return(
         <section>
             <div className='perfil' id='profile'>
                 <img src="/FotoGui/foto_guilherme.jpg" alt="Guilherme" />
+                <div className='perfil-texto'>
+                <h2>{textPerfil}</h2>
                 <p>Buscando unir lógica e design para criar experiências digitais únicas.</p>
+                </div>
             </div>
         </section>
     )
